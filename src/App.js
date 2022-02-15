4eed9ade3c7ef2406580db6b672c3bd992632534
Navya923 <navyaramesh333@gmail.com>
2022-02-15 16:37:44 +0530

    food

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import Resturants from './components/Resturants';
 import Products from './components/Products';
-// import AllProducts from './products.json';
+import Resturants from './components/Resturants';
 import Cart from './components/Cart';
 import filterList from './components/filterList';
 
@@ -11,6 +10,7 @@ import filterList from './components/filterList';
 const App = () => {
 
   const [products, setProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
   const [selectedResturants, setSelectedResturants] = useState([]);
 
   const [cart, setCart] = useState([]);
@@ -24,46 +24,38 @@ const App = () => {
         .then((response) => {
           console.log(response);
           setProducts(response.data);
+          setAllProducts(response.data);
           setMountFlag(true)
         }).catch((err) => {
           console.log(err);
         })
     }
   }, [])
-  
-
-  
 
   const setResturant = (resturant) => {
-    console.log(resturant);
-  
-    console.log(products);
-    let newArray=[];
-    if(resturant.name=='AllProducts'){
-      newArray=products
-    }else{
-      newArray = products.filter((element) => {
+    let newArray = [];
+    if (resturant.name == 'AllProducts') {
+      newArray = allProducts;
+    } else {
+      newArray = allProducts.filter((element) => {
         if (element.restaurantId == resturant.id) {
           return element;
         }
       })
     }
-    
-    console.log(newArray)
-
     setProducts(newArray);
   }
-  
 
-  const searchProducts=(text)=>{
+
+  const searchProducts = (text) => {
     // console.log('searchtext',text);
-    let searchText=text.toLocaleLowerCase()
-    let filteredProducts=Products.filter((product)=>{
-      let productName=product.name.toLocaleLowerCase();
-      let restaurantName=product.restaurantName.toLocaleLowerCase();
-      if(productName.includes(searchText) || restaurantName.includes(searchText)){
+    let searchText = text.toLocaleLowerCase()
+    let filteredProducts = allProducts.filter((product) => {
+      let productName = product.name.toLocaleLowerCase();
+      let restaurantName = product.restaurantName.toLocaleLowerCase();
+      if (productName.includes(searchText) || restaurantName.includes(searchText)) {
         return product;
-      }  
+      }
     });
     // console.log(filteredProducts);
     setProducts(filteredProducts);
@@ -94,74 +86,74 @@ const App = () => {
     const index = productList.indexOf(item);
     productList[index].quantity++;
     setCart(productList);
-
     console.log(item);
     axios.post("http://localhost:8080/Cart/cart", {
-      
-        "product": 
-            {
-                "id": item.id,
-                "quantity": item.quantity++,
-                "price": item.price,
-                "name": item.name,
-                "discount": item.discount,
-                "restaurantId": item.restaurantId,
-                "category": item.category,
-                "topPick": item.topPick,
-                "cart": item.cart,
-                "url": item.url
-            }
-        
-      }
+      "product":
+
+      {
+        "id": item.id,
+        "quantity": item.quantity,
+        "price": item.price,
+        "name": item.name,
+        "discount": item.discount,
+        "restaurantId": item.restaurantId,
+        "category": item.category,
+        "topPick": item.topPick,
+        "cart": item.cart,
+        "url": item.url
+      },
+      "quantity":1
+
+    }
 
     ).then((response) => {
       console.log(response);
     })
-    .catch((error) => {
+      .catch((error) => {
         console.log(error);
-    })
+      })
   }
 
   const changeQuantity = (item, e) => {
     const productList = [...cart];
-    console.log(item,e);
+    console.log(item, e);
     const index = productList.indexOf(item);
     if (e === '+') {
       productList[index].quantity++;
       //update quantity of item
-      axios.put('http://localhost:8080/Cart/cart',{
-        id:'',
+      axios.put('http://localhost:8080/Cart/cart', {
+        id: '',
       })
-      .then((Response)=>{
-        console.log(Response);
-      }).catch((Err)=>{
-        console.log(Err);
-      })
+        .then((Response) => {
+          console.log(Response);
+        }).catch((Err) => {
+          console.log(Err);
+        })
     }
     else {
       if (productList[index].quantity > 0) {
         productList[index].quantity--;
         //Remove item from Database
-        axios.put('http://localhost:8080/Cart/cart',{
-          id:'',
+        axios.put('http://localhost:8080/Cart/cart', {
+          id: '',
         })
-        .then((Response)=>{
-          console.log(Response);
-        }).catch((Err)=>{
-          console.log(Err);
-        })
+          .then((Response) => {
+            console.log(Response);
+          }).catch((Err) => {
+            console.log(Err);
+          })
       }
       else {
         productList.splice(index, 1);
         //delete API
-        axios.delete('http://localhost:8080/Cart/cart',{
-          id:'',
+        axios.delete('http://localhost:8080/Cart/cart', {
+          id: '',
         })
-        .then((Response)=>{
-          console.log(Response);
-        }).catch((Err)=>{
-          console.log(Err);
-        })
+          .then((Response) => {
+            console.log(Response);
+          }).catch((Err) => {
+            console.log(Err);
+          })
       }
     }
     setCart(productList);
@@ -169,36 +161,36 @@ const App = () => {
 
   const handleClearProducts = () => {
     console.log('clear', cart);
-    let CartItems=[];
+    let CartItems = [];
     axios.get('http://localhost:8080/Cart/cart')
-    .then((response)=>{
-      console.log(response);
-      CartItems=response.data;
-    }).catch((err)=>{
-      console.log(err);
-    })
-    CartItems.map((element)=>{
-      axios.delete('http://localhost:8080/Cart/cart',{
-        id:element.id,
+      .then((response) => {
+        console.log(response);
+        CartItems = response.data;
+      }).catch((err) => {
+        console.log(err);
       })
-      .then((Response)=>{
-        console.log(Response);
-      }).catch((Err)=>{
-        console.log(Err);
+    CartItems.map((element) => {
+      axios.delete('http://localhost:8080/Cart/cart', {
+        id: element.id,
       })
+        .then((Response) => {
+          console.log(Response);
+        }).catch((Err) => {
+          console.log(Err);
+        })
     })
-    
+
     setCart([]);
   }
 
-  
+
 
   return (
     <div className="App">
-     <div class="header">
- 
- <center> <img src="https://image.freepik.com/free-vector/chinese-food-background-illustrated_52683-68274.jpg"/></center>
-</div>
+      <div class="header">
+
+        <center> <img src="https://image.freepik.com/free-vector/chinese-food-background-illustrated_52683-68274.jpg" /></center>
+      </div>
 
       <Resturants selectedResturants={selectedResturants} setResturant={setResturant} />
       <center><Products searchProducts={searchProducts} products={products} sortProducts={sortProducts} addToCart={addToCart} /></center>
--- src/components/Resturants.js
+++ src/components/Resturants.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 
-const Resturants = ({selectedResturants, setResturant}) =>  {
-console.log(selectedResturants)
-        const resturants =
+const Resturants = ({ selectedResturants, setResturant }) => {
+    const resturants =
         [
-            {"id" : 0,
-            "name": "AllProducts"},
-        
-            {"id" : 1,
-                    "name": "Empire"},
-        
-            {"id" : 2,
-                    "name": "A2B"},
-        
-            {"id" : 3,
-                    "name": "Taj"},
-            {"id" : 4,
-                    "name": "McDonalds"},
-        
-                    ]
+            {
+                "id": 0,
+                "name": "AllProducts"
+            },
+            {
+                "id": 1,
+                "name": "Empire"
+            },
+            {
+                "id": 2,
+                "name": "Taj"
+            },
+            {
+                "id": 3,
+                "name": "McDonalds"
+            },
+            {
+                "id": 4,
+                "name": "A2B"
+            }
+        ]
     return (
         <div className="resturants">
             <center><h1>TOP RESTURANTS</h1></center>
@@ -27,15 +32,15 @@ console.log(selectedResturants)
                 {
                     resturants.map((resturant, index) => {
                         return (
-                            <Button 
-                                className={ "resturant" + (selectedResturants.includes(resturant.name) ? " selected-resturant" : "")}
+                            <Button
+                                className={"resturant" + (selectedResturants.includes(resturant.name) ? " selected-resturant" : "")}
                                 key={index}
                                 onClick={() => setResturant(resturant)}
                             >
                                 {resturant.name}
                             </Button>
                         )
-                        
+
                     })
                 }
             </div>
@@ -44,3 +49,4 @@ console.log(selectedResturants)
 }
 
 export default Resturants;
+
